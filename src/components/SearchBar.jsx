0724d54c9d4ccee8bc3280e392a2f12f42df519b
obulbuk07 <obulbuk07@gamil.com>
2026-08-17 15:39:42 +0200

    customise Search component

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,10 @@
+import searchIcon from "../img/search_icon.png"
+
 function SearchBar({ query, onQueryChange, onSearch }){
     return(
-        <div className="w-2/4 mx-auto flex items-center flex-col py-30">
-            <h2 className="text-2xl text-gray-700">Search what are you interesting about?</h2>
-            <p className="pt-5 text-gray-400">enter key words here</p>
+        <div className="w-3/4 mx-auto flex items-center flex-col py-30">
+            <h2 className="text-3xl pb-2 text-gray-700">Знайди те, що тобі потрібно</h2>
+            <p className="pt-5 text-gray-400 pb-6">Введи ключові слова</p>
             <div className="w-full flex flex-row gap-4">
                 <input
                     className="w-4/4 py-2 px-4 mt-4 rounded-xl border-3 shadow-xl border-gray-400 focus:border-blue-500"
@@ -10,7 +12,7 @@ function SearchBar({ query, onQueryChange, onSearch }){
                     onChange={(e) => onQueryChange(e.target.value)}
                     type="text"
                 />
-                <button onClick={onSearch} className="border py-2 px-4 mt-4 rounded">Seach</button>
+                <img className=" w-7 h-7 mt-6" src={searchIcon} alt="" />
             </div>
         </div>
     )
