d5d12edf970c5a8bc1dc437092a7c4f7f752cf48
obulbuk07 <obulbuk07@gamil.com>
2026-08-17 16:01:57 +0200

    Change home -> target, studentGuide block

--- src/components/StudentGuideBlock.jsx
+++ src/components/StudentGuideBlock.jsx
@@ -4,10 +4,10 @@ import studentguide from "../img/student_guide_logo.jpg"
 function StudentGuideBlock(){
     return(
         <div className="grid grid-cols-5 gap-4 col-span-2 bg-red-400 p-4 rounded-xl shadow-xl text-white" >
-            <img className="bg-white w-40 h-40 rounded-xl p-2 col-span-2  " src={studentguide} alt="" />
+            <img className="bg-white w-42 h-42 rounded-xl p-2 col-span-2  " src={studentguide} alt="" />
             <div className="col-span-3" >
-                <h2 className="col-span-3 text-xl pb-3" >Student Guide</h2>
-                <p className="col-span-3" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, unde!</p>
+                <h2 className="col-span-3 text-xl pb-1" >Student Guide</h2>
+                <p className="col-span-3" >Student guide допоможе першокурсникам швидко знайти необхідну інформацію про навчання, послуги та можливості нашого медичного коледжу й Університету.</p>
             </div>
            
             <Link className="col-span-5 text-center border-3 rounded-xl border-white flex items-center justify-center text-xl py-2 hover:bg-white hover:text-red-400 transition" to="/guide">Перейти</Link>
--- src/components/Target.jsx
+++ src/components/Target.jsx
@@ -2,10 +2,10 @@ function Target(){
     return(
         <div className="col-span-2 flex flex-col gap-2 shadow-xl rounded-xl bg-green-400 py-8 px-4">
             <h1 className="text-xl text-white text-center pb-4">Цільовою аудиторією освітнього хабу є:</h1>
-            <p className="py-4 px-6 bg-gray-100 rounded-full shadow-sm" >студенти фахового медичного коледжу ІФНМУ спеціальності 223 Медсестринство</p>
-            <p className="py-2 px-4 bg-gray-100 rounded-full shadow-sm" >студенти фахового медичного коледжу ІФНМУ інших спеціальностей</p>
-            <p className="py-2 px-4 bg-gray-100 rounded-full shadow-sm" >студенти медичного факультету ІФНМУ</p>
-            <p className="py-2 px-4 bg-gray-100 rounded-full shadow-sm" >студенти інших медичних закладів освіти</p>
+            <p className="py-4 px-6 bg-green-500 text-white rounded-full shadow-sm" >Студенти фахового медичного коледжу ІФНМУ спеціальності 223 Медсестринство</p>
+            <p className="py-2 px-4 bg-green-500 text-white rounded-full shadow-sm" >Студенти фахового медичного коледжу ІФНМУ інших спеціальностей</p>
+            <p className="py-2 px-4 bg-green-500 text-white rounded-full shadow-sm" >Студенти медичного факультету ІФНМУ</p>
+            <p className="py-2 px-4 bg-green-500 text-white rounded-full shadow-sm" >Студенти інших медичних закладів освіти</p>
         </div>
     )
 }
