ec85fde668549d1874917f44ba1974ee87482c15
obulbuk07 <obulbuk07@gamil.com>
2026-08-28 13:43:00 +0200

    Remove orbital images from Student Guide

--- src/components/StudentOrgs.jsx
+++ src/components/StudentOrgs.jsx
@@ -1,9 +1,8 @@
-import OrbitalLogo from "./OrbitalLogo"
+
 function StudentOrgs(){
     return(
         <div className="flex flex-col w-3/4 mx-auto">
             <h1 className="text-center text-3xl my-20" >Студентські організації</h1>
-            <OrbitalLogo/>
         </div>
     )
 }
